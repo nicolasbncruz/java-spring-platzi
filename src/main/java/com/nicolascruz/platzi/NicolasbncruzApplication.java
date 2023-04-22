c0df5bf90613aa0d59c5e5f1f49daaa3e923f08b
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2023-04-21 23:44:12 -0500

    first commit

---
+++ src/main/java/com/nicolascruz/platzi/NicolasbncruzApplication.java
@@ -0,0 +1,13 @@
+package com.nicolascruz.platzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolasbncruzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolasbncruzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolascruz/platzi/NicolasbncruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolascruz.platzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasbncruzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
